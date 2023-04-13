11007b3aab780bdebd18d3ad144683e61c450de9
IceAmerican0 <starsjun@naver.com>
2023-04-13 17:41:54 +0900

    adding clickedEmptyCell logic

--- TheHurtLocker/TheHurtLocker/View/MineFieldView.swift
+++ TheHurtLocker/TheHurtLocker/View/MineFieldView.swift
@@ -16,22 +16,26 @@ struct MineFieldView: View {
             ForEach(0..<board.height, id: \.self) { row in
                 HStack {
                     ForEach(0..<board.width, id: \.self) { column in
-                        let isClicked = board.cells[row][column]
+                        let selectedCell = board.cells[row][column]
                         Button(action: {
-                            if isClicked.isRevealed || isClicked.isFlagged {
+                            if selectedCell.isRevealed || selectedCell.isFlagged {
                                 return
                             }
                             
-                            if isClicked.isMine {
+                            if selectedCell.isMine {
                                 isGameOver = true
                             }
                             
                             board.cells[row][column].isRevealed = true
+                            
+                            if selectedCell.neighboringMines == 0 {
+                                NeighboringEmptyCell(row: row, column: column)
+                            }
                         }, label: {
-                            if isClicked.isFlagged {
-                                Image(systemName: "flag")
-                            } else if isClicked.isRevealed {
-                                Image(systemName: isClicked.isMine ? "circle.fill" : "1.circle")
+                            if selectedCell.isFlagged {
+                                Image(systemName: "flag.fill")
+                            } else if selectedCell.isRevealed {
+                                Image(systemName: selectedCell.isMine ? "circle.fill" : "\(String(selectedCell.neighboringMines)).circle")
                             } else {
                                 Image(systemName: "circle")
                             }
@@ -40,6 +44,23 @@ struct MineFieldView: View {
                                 LongPressGesture().onEnded({ _ in board.cells[row][column].isFlagged.toggle() })
                             )
                     }
+                }
+            }
+        }
+    }
+    
+    func NeighboringEmptyCell(row: Int, column: Int) {
+        for neighboringRow in -1...1 {
+            for neighboringColumn in -1...1 {
+                if row + neighboringRow < 0 || row + neighboringRow >= board.width || column + neighboringColumn < 0 || column + neighboringColumn >= board.height {
+                    continue
+                }
+                
+                let row = row + neighboringRow
+                let column = column + neighboringColumn
+                if board.cells[row][column].neighboringMines == 0 {
+                    board.cells[row][column].isRevealed = true
+//                    self.NeighboringEmptyCell(row: row, column: column)
                 }
             }
         }
